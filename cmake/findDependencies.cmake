b8d0565d5ae4c3106695f1f1c9b904c7e6f4ffa6
Thibault Vatter <thibault.vatter@gmail.com>
2024-02-23 16:55:32 +0100

    add documentation (#11)
    * add documentation
    
    * correct CI

--- cmake/findDependencies.cmake
+++ cmake/findDependencies.cmake
@@ -2,3 +2,18 @@ find_package(Eigen3                       REQUIRED)
 find_package(Boost 1.56                   REQUIRED)
 
 set(external_includes ${EIGEN3_INCLUDE_DIR} ${Boost_INCLUDE_DIRS})
+
+# Find doxygen and configure if found
+find_package(Doxygen QUIET)
+if(DOXYGEN_FOUND)
+    configure_file(
+        ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile.in
+        ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY
+    )
+    add_custom_target(doc
+        ${DOXYGEN_EXECUTABLE}
+        ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        COMMENT "Generating API documentation with Doxygen" VERBATIM
+    )
+endif (DOXYGEN_FOUND)
